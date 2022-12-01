5027fea47fd26e0eda7e775284e6d6e54a415dc3
Sam McGeown <smcgeown@vmware.com>
2022-12-01 16:00:41 +0000

    Add dark/light mode toggle

--- static/js/clr-toggle-darkmode.js
+++ static/js/clr-toggle-darkmode.js
@@ -1,41 +1,38 @@
-function toggleThemeMode() {
+function toggleThemeMode(mode) {
     var lightModeCss = document.getElementById("light-mode-css");
     var darkModeCss = document.getElementById("dark-mode-css");
     var toggleIcon = document.getElementById("icon-theme-mode");
 
-    if(lightModeCss.disabled) {
+    if(mode == undefined) {
+        if(toggleIcon.getAttribute("shape") == "sun") {
+            mode = "light"
+        } else if (toggleIcon.getAttribute("shape") == "moon"){
+            mode = "dark"
+        }
+    }
+
+    if(mode == "light") {
+        // Enable light mode
+        console.log('enable light mode')
         darkModeCss.disabled = true;
         lightModeCss.disabled = false;
         toggleIcon.setAttribute("shape", "moon");
+        localStorage.setItem("clarity-theme-mode", "light");
     } else {
+        console.log('enable dark mode')
         darkModeCss.disabled = false;
         lightModeCss.disabled = true;
         toggleIcon.setAttribute("shape", "sun");
+        localStorage.setItem("clarity-theme-mode", "dark");
     }
-  }
+    console.log(localStorage.getItem('clarity-theme-mode'))
 
+}
 
-// const darkIconClass = 'moon'
-// const lightIconClass = 'sun'
-// var darkCSS = $("#dark-mode-css");
-// var darkToggleIcon = $("#icon-theme-mode");
-
-// // Set theme with the given mode and toggle the theme icon button
-// function setThemeMode(mode) {
-//   if (mode === "dark") {
-//     darkCSS.prop('disabled', false)
-//     //$("#dark-toggle i").attr('class', lightIconClass)
-//   } else if (mode === "light") {
-//     darkCSS.prop('disabled', true)
-//     //darkToggleIcon.attr('class', darkIconClass)
-//   }
-// }
-
-// // Toggle dark mode when user click on the icon button
-// $("#toggle1").click(function () {
-//   if (darkToggleIcon.attr("shape") == darkIconClass) {
-//     setThemeMode("dark")
-//   } else if (darkToggleIcon.attr("shape") == lightIconClass) {
-//     setThemeMode("light")
-//   }
-// });
+function setCurrentTheme() {
+    const currentTheme = localStorage.getItem('clarity-theme-mode') ? localStorage.getItem('clarity-theme-mode') : null;
+    if(currentTheme) {
+        console.log(currentTheme)
+        toggleThemeMode(currentTheme)
+    }
+}
